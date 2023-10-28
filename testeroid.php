2b70a117065646575acb5ea03433acb57588a61b
Anatolii Iumashev <a.yumashev@gmail.com>
2023-10-28 17:27:03 +0300

    improve CLI

--- testeroid.php
+++ testeroid.php
@@ -1,89 +1,132 @@
-<?php 
+<?php
 /**
  * Plugin Name: Testeroid
  * Description: TDD and simple auto tests with WP CLI
- * Version: 0.7
+ * Version: 0.8
  */
 
 namespace Testeroid;
 
-use WP_CLI;
+use WP_CLI, Throwable;
 
-if(class_exists('WP_CLI')){
-    WP_CLI::add_command( 'test', function($terms, $args){
-        $results = testing($terms, $args);
-        if($results['success'] && empty($results['fails'])){
-            WP_CLI::success( 'tests success' );
-        } else {
-            WP_CLI::error( 'tests fails', $exit = false );
+if ( class_exists( 'WP_CLI' ) ) {
+	WP_CLI::add_command( 'test', function ($terms, $args) {
+		$results = testing( $terms, $args );
+        
+        WP_CLI::log(sprintf('success: %s', $results['success']));
+        if(!empty($results['fails'])){
+            WP_CLI::log(sprintf('fails: %s', $results['fail']));
+            WP_CLI::error_multi_line( $results['fails'] );
         }
-        var_dump($results);
-        exit;
-    } );
+
+        if ( $results['success'] && empty( $results['fails'] ) ) {
+			WP_CLI::success( 'tests success' );
+		} else {
+			WP_CLI::error( 'tests fails', $exit = false );
+		}
+
+	} );
 
 }
 
-function testing($terms, $args){
+function testing( $terms, $args ) {
 
-    global $test_results;
-    
-    if(empty($test_results)){
-        $test_results = [
-            'success' => 0,
-            'fail' => 0,
-            'fails' => [],
-        ];
-    }
+	global $test_results, $testerod_tests;
 
-    $path = __DIR__ . '/includes/';
-    if(defined('TESTEROID_TESTS_PATH')){
-        $path = trailingslashit(TESTEROID_TESTS_PATH);
-    }
 
-    if(isset($terms[0])){
-        $term = $terms[0];
-        if( str_ends_with($term, '.php')){
-            $path_pattern_test = $path_pattern = trailingslashit($path) . $term;
-            if(file_exists($path_pattern_test)){
-                require_once $path_pattern_test;
-            } else {
-                WP_CLI::error( 'Tests no found: ' . $path_pattern_test, $exit = false );
-            }
-        } else {
-            WP_CLI::error( 'File name shoud be php format. ' . $term, $exit = false );
-        }
-    } else {
-        $path_pattern = trailingslashit($path) . '*.php';
+	$path = __DIR__ . '/includes/';
+	if ( defined( 'TESTEROID_TESTS_PATH' ) ) {
+		$path = trailingslashit( TESTEROID_TESTS_PATH );
+	}
 
-        foreach(glob($path_pattern) as $php_include) {
-            require_once($php_include);
-        }
-    }
+	if ( isset( $terms[0] ) ) {
+		$term = $terms[0];
+		if ( str_ends_with( $term, '.php' ) ) {
+			$path_pattern_test = $path_pattern = trailingslashit( $path ) . $term;
+			if ( file_exists( $path_pattern_test ) ) {
+				require_once $path_pattern_test;
+			} else {
+				WP_CLI::error( 'Tests no found: ' . $path_pattern_test, $exit = false );
+			}
+		} else {
+			WP_CLI::error( 'File name shoud be php format. ' . $term, $exit = false );
+		}
+	} else {
+		$path_pattern = trailingslashit( $path ) . '*.php';
 
-    
+		foreach ( glob( $path_pattern ) as $php_include ) {
+			require_once( $php_include );
+		}
+	}
 
-    return $test_results;
+	if ( empty( $testerod_tests ) ) {
+		WP_CLI::error( 'No tests. ' . $term, $exit = false );
+	}
+
+
+	$progress = \WP_CLI\Utils\make_progress_bar( 'Testing', count( $testerod_tests ), $interval = 1 );
+
+    if ( empty( $test_results ) ) {
+		$test_results = [ 
+			'success' => 0,
+			'fail' => 0,
+			'fails' => [],
+		];
+	}
+
+	foreach ( $testerod_tests as $test ) {
+		$progress->tick();
+
+		if ( $test['active'] ) {
+
+			try {
+				$result = call_user_func( $test['callback'] );
+				if ( $result ) {
+					$test_results['success']++;
+				} else {
+					$test_results['fail']++;
+
+                    $msg = $test['text'];
+
+                    if(isset($test['debug_backtrace'][0])){
+                        $log = $test['debug_backtrace'][0];
+                        $msg .= ' | File: ' . $log['file'] . ":" . $log['line'];
+                    }
+
+					$test_results['fails'][] = $msg;
+
+				}
+			} catch (Throwable $th) {
+				$test_results['fail']++;
+				$test_results['fails'][] = $test['text'] . '; ' . $th->getMessage() . '; ' . $th->getFile() . ':' . $th->getLine();
+
+                
+			}
+		}
+	}
+
+	$progress->finish();
+
+	// ddcli($testerod_tests);
+
+
+	return $test_results;
 }
 
 
-function test($text, $function, $active = true){
-    
-    global $test_results;
-    
-    if($active){
-        try {
-            $result = call_user_func($function);
-            if($result){
-                $test_results['success']++;
-            } else {
-                $test_results['fail']++;
-                $test_results['fails'][] = $text;
-            }
-        } catch (\Throwable $th) {
-            $test_results['fail']++;
-            $test_results['fails'][] = $text . '; ' . $th->getMessage() . '; ' . $th->getFile() . ':' . $th->getLine();
-        }   
-    }
+function test( $text, $function, $active = true ) {
+
+	global $testerod_tests;
+	if ( empty( $testerod_tests ) ) {
+		$testerod_tests = [];
+	}
+
+	$testerod_tests[] = [ 
+		'text' => $text,
+		'callback' => $function,
+		'active' => $active,
+		'debug_backtrace' => debug_backtrace(),
+	];
 }
 
 
@@ -97,23 +140,23 @@ function transaction_query( $type = 'start' ) {
 	$wpdb->hide_errors();
 
 	switch ( $type ) {
-        case 'commit':
-            $wpdb->query( 'COMMIT' );
-            break;
-        case 'rollback':
-            $wpdb->query( 'ROLLBACK' );
-            break;
-        default:
-            $wpdb->query( 'START TRANSACTION' );
-            break;
-    }
+		case 'commit':
+			$wpdb->query( 'COMMIT' );
+			break;
+		case 'rollback':
+			$wpdb->query( 'ROLLBACK' );
+			break;
+		default:
+			$wpdb->query( 'START TRANSACTION' );
+			break;
+	}
 }
 
 
-function ddcli(...$vars){
-    foreach($vars as $key => $var){
-        print_r($var, false);
-        echo PHP_EOL;
-    }
-    exit;
+function ddcli( ...$vars ) {
+	foreach ( $vars as $key => $var ) {
+		print_r( $var, false );
+		echo PHP_EOL;
+	}
+	exit;
 }
